63ff2cea35de301745880f2c25f2b1dc1f8afab4
Misha <mishachernyshov2@gmail.com>
2021-04-29 00:26:49 +0300

    dsf

--- smartFurnitureConstructor/custom_azure.py
+++ smartFurnitureConstructor/custom_azure.py
@@ -2,12 +2,12 @@ from storages.backends.azure_storage import AzureStorage
 
 class AzureMediaStorage(AzureStorage):
     account_name = 'smartconstructorstorage' # Must be replaced by your <storage_account_name>
-    account_key = '01GtJjOaEeNRdCSkh6xFfU2PSpx8OU7jNqzghNjVAY632sc38d8Hib7DPbF6ODcSkQyLvesMh96n6RKsjbY6lw==' # Must be replaced by your <storage_account_key>
+    account_key = '9Crrc3cj6XgIIA2cFb6a4PXhFRGTN+kjvghcQDq/sDxh5DEzDQ14/1xgUyURgE/u7dPq8QVKVHAnJ448Qteq7g==' # Must be replaced by your <storage_account_key>
     azure_container = 'media'
     expiration_secs = None
 
 class AzureStaticStorage(AzureStorage):
     account_name = 'smartconstructorstorage' # Must be replaced by your storage_account_name
-    account_key = '01GtJjOaEeNRdCSkh6xFfU2PSpx8OU7jNqzghNjVAY632sc38d8Hib7DPbF6ODcSkQyLvesMh96n6RKsjbY6lw==' # Must be replaced by your <storage_account_key>
+    account_key = '9Crrc3cj6XgIIA2cFb6a4PXhFRGTN+kjvghcQDq/sDxh5DEzDQ14/1xgUyURgE/u7dPq8QVKVHAnJ448Qteq7g==' # Must be replaced by your <storage_account_key>
     azure_container = 'static'
     expiration_secs = None
